39945207c78a3210a2d97e3a43b3060683823e99
bruhtus <hibruhtus@gmail.com>
2021-08-24 11:13:07 +0700

    Add first component from udemy course
    Ref: https://www.udemy.com/course/the-complete-react-native-and-redux-course/

---
+++ App.js
@@ -0,0 +1,19 @@
+import { createAppContainer } from "react-navigation";
+import { createStackNavigator } from "react-navigation-stack";
+import HomeScreen from "./src/screens/HomeScreen";
+import CounterScreen from "./src/screens/CounterScreen";
+
+const navigator = createStackNavigator(
+  {
+    Home: HomeScreen,
+    Counter: CounterScreen,
+  },
+  {
+    initialRouteName: "Home",
+    defaultNavigationOptions: {
+      title: "App",
+    },
+  }
+);
+
+export default createAppContainer(navigator);
